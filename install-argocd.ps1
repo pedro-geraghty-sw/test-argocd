f66faff21c8638097a16c8b5c0b5c74054e8a9c7
Pedro Geraghty Fleming <pedro.geraghty@southworks.com>
2023-08-31 12:29:36 -0300

    ArgoCD with private repo

--- install-argocd.ps1
+++ install-argocd.ps1
@@ -1,2 +1,3 @@
 helm install argocd  argo/argo-cd -n argocd --create-namespace
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | wsl base64 -d
+argocd repo add https://github.com/southworks/rukbat --username $Env:SW_GITHUB_USER --password $Env:SW_GITHUB_TOKEN --port-forward
